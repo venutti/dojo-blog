575fc06b5c6661294f220153936487b290fa8d2d
venutti <lvenutti@fi.uba.ar>
2023-01-19 14:09:19 -0300

    Agrega los componentes Navbar y Home

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <div>
+        <a href="/">Home</a>
+        <a href="/create">New Post</a>
+      </div>
+    </nav>
+  );
+}
